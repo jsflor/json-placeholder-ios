0d1ef2099f6f6554cd8a9eb0a73a88001ed1555c
Sebastian Flor <sebastian.flor@atheneum.ai>
2023-08-04 14:01:51 +0200

    Initial Commit

---
+++ Json Placeholder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Json Placeholder
+//
+//  Created by Sebastian Flor on 04.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Json Placeholder/Json_PlaceholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Json_PlaceholderApp.swift
+//  Json Placeholder
+//
+//  Created by Sebastian Flor on 04.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Json_PlaceholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
